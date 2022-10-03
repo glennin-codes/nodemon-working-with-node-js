55e34634b3ba241ce59423263998a6c9700262cd
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-03 11:48:24 +0300

    passing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 const {format} = require('date-fns');
 const {v4:uuid}=require('uuid')
- console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'));
- console.log(uuid());
+
+ const path =require('path')
+ const fs =require('fs');
+ const fsPromises=require('fs').promises
+
+ const logEvents=async(messages)=>{
+    const  dateTime= `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}` 
+    const logItem=`${dateTime}\t ${uuid}\t ${messages}`
+    try{
+        if (!fs.existsSync(path.join(__dirname,'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname,'logs','log.txt'),logItem)
+    }catch (err){
+        console.error(err);
+    }
+ }
  
