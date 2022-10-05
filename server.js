8f408a64dc80732f773d3a67e595f4922e78a14b
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-05 09:10:05 +0300

    working with server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http=require('http');
 const { createServer } = require('https');
-// const path =require('path')
-// const fs=require('fs')
-// const fsPromise=require('fs').promises
+const path =require('path')
+const {fs ,readFileSync, rmSync}=require('fs')
+const fsPromise=require('fs').promises
 
 // const logEvents =require('./logEvents')
 // const EventEmitter= require ('events')
@@ -16,13 +16,51 @@ const { createServer } = require('https');
 
 const PORT= process.env.PORT || 3500;
 const server= http.createServer((req,res)=>{
-    res.write('hello world');
-    res.end();
- 
+    res.writeHead(200,{'content-Type': 'text/html'})
+ if(req.url===path.join(__dirname,'views','index')){
+   const home=async()=>{
+    try{
+          const data =await fsPromise.readFile(path.join(__dirname,'views','index'),'utf8')
+         res.end(data)
+    }catch (err){
+     console.log(err);
+    }
+    
+ }
+ home()
+
+}else if (req.url===path.join(__dirname ,'views' ,'about us')){
+    const aboutUs=async ()=>{
+        try{
+            const data2=await fsPromise.readFile(path.join(__dirname ,'views' ,'about us'),'utf8')
+           res.end(data2)
+        }catch(err){
+           console.log(err);
+        }
+    } 
+     aboutUs();
+}else{
+    const outPage= async()=>{
+        try{
+             readFileSync(path.join(__dirname,'views','404'),'utf8',(err,dataEr)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    const data3=dataEr
+                }
+            })
+            res.end(data3)
+        }catch(err){
+            console.error(err);
+
+        }
+    } 
+    outPage()
+}
 })
 server.listen(PORT, (err)=>{
     if (err){
-        console.log('the was an problem '+ err);
+        console.log('the was a problem '+ err);
     }
      else{
         console.log('server running at port ' + PORT);
