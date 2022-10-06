9452bcf9f9494a0368179823a475252ccd3ca835
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-06 08:10:00 +0300

    mind grunade

--- server.js
+++ server.js
@@ -1,68 +1,25 @@
-const http=require('http');
-const { createServer } = require('https');
-const path =require('path')
-const {fs ,readFileSync, rmSync}=require('fs')
-const fsPromise=require('fs').promises
+const http=require('http')
+const Path =require ('path')
+const fs =require('fs')
+const fsPromise=require ('fs')
+//the port to be used
+const PORT= process.env.PORT||3000
 
-// const logEvents =require('./logEvents')
-// const EventEmitter= require ('events')
-// class Emitter extends EventEmitter{};  
-// //initialize object
-// const myEmitter=new Emitter();
-// //add listener for the log event
-// //  myEmitter.on('log',(msg)=>logEvents(msg))
- 
-// //     myEmitter.emit('log','log event emitted')
-
-const PORT= process.env.PORT || 3500;
 const server= http.createServer((req,res)=>{
-    res.writeHead(200,{'content-Type': 'text/html'})
- if(req.url==='./'){
-   const home=async()=>{
-    try{
-          const data =await fsPromise.readFile(path.join(__dirname,'views','index'),'utf8')
-         res.end(data)
-    }catch (err){
-     console.log(err);
+    console.log(req.url,req.method);
+    let path;
+    if (req.url==='/' || req.url==='index.html'){
+        res.statusCode=200;
+        res.setHeader('content-Type','text/html' ,'text/css')
+        path=Path.join(__dirname, 'views','index.html')
+        fs.readFile(path, 'utf8',(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.end(data)
+            }
+        })
     }
-    
- }
- home()
-
-}else if (req.url==='./about'){
-    const aboutUs=async ()=>{
-        try{
-            const data2=await fsPromise.readFile(path.join(__dirname ,'views' ,'about us'),'utf8')
-           res.end(data2)
-        }catch(err){
-           console.log(err);
-        }
-    } 
-     aboutUs();
-}else{
-    const outPage= async()=>{
-        try{
-             const data3 = fs.writeFile(path.join(__dirname,'views','404.html'),'page not found',(err)=>{
-                if(err){
-                    console.log(err);
-                }else{
-                    res.end(data3)
-                }
-            })
-            
-        }catch(err){
-            console.error(err);
-
-        }
-    } 
-    outPage()
-}
 })
-server.listen(PORT, (err)=>{
-    if (err){
-        console.log('the was a problem '+ err);
-    }
-     else{
-        console.log('server running at port ' + PORT);
-     }
-})
+server.listen(PORT,()=>console.log(`starting server at ${PORT}`))
+
