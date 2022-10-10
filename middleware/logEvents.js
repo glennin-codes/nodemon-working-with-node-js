d89e0e652b39fd20555031888130750ae9ac2d56
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-10 13:32:12 +0300

    custom middlware

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -6,15 +6,15 @@ const { v4:uuid}= require('uuid')
  const fs =require('fs');
  const fsPromises=require('fs').promises
 
- const logEvents  = async(messages)=> {
+ const logEvents  = async(messages,logName)=> {
     const  dateTime= `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}` 
     const logItem=`${dateTime}\t${uuid()}\t ${messages}\n`
     console.log(logItem);
     try{
-        if (!fs.existsSync(path.join(__dirname,'logs'))){
+        if (!fs.existsSync(path.join(__dirname,'..','logs'))){
             await fsPromises.mkdir(path.join(__dirname,'logs'))
         }
-        await fsPromises.appendFile(path.join(__dirname,'logs','eventLogs.txt'),logItem)
+        await fsPromises.appendFile(path.join(__dirname,'..','logs',logName),logItem)
     }catch (err){
         console.error(err);
     }
