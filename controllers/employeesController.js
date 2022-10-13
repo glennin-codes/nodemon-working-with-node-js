78b4c02e652dabb575c0a7b3c23b368d75b61a3c
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-13 16:51:57 +0300

    deleting

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,3 +1,5 @@
+const { parse } = require('date-fns');
+
 const data ={
   employees:require('../model/employees.json'),
   setEmployees:function(data){
@@ -22,16 +24,30 @@ const readingEmployees= (req,res)=>{
       res.status(201). json(data.employees)
   }
   const updatingEmployees=(req,res)=>{
-    res.json({
-        "firstname" :req.body.firstname,
-        "lastname"  : req.body.lastname
-    })
+    const employee=data.employees.find(emp=>{emp.id===parseInt(req.body.id)});
+    if(!employee){
+      return res.status(400).json({"message":`employee  ${req.body.id}  cant' be found`});
+    }
+    if(req.body.firstname) employee.firstname=req.body.firstname;
+    if(req.body.lastname) employee.lastname=req.body.lastname;
+    const filterdArray=data.employees.filter(emp=>emp.id !== parseInt(req.body.id))
+    const unSortedArray=[...filterdArray,employee];
+    data.setEmployees(unSortedArray.sort((a,b)=>a.id>b.id?1:a.id<b.id?-1:0))  
+    
+    res.status(201).json(data.employees)
   }
   const deletingEmployees=(req,res)=>{
-    res.json({"id":req.body.id})
-  }
+    const employee=data.employees.find(emp=>emp.id===parseInt(req.body.id));
+    if(!employee){
+      res.status(401).json({"messages":`employee ${req.body.id} cant be found`});
+
+    }
+      const filterdArray=data.employees.filter(emp=>emp.id !==parseInt(req.body.id));
+      data.setEmployees([...filterdArray])
+      res.json(data.employees)
+    }
   const readingParticularEmp=(req,res)=>{
-    res.json({"id":req.body.id})
+
   }
   
 
