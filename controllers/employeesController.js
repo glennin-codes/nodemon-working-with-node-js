f1506a2466267ba9b1a9f2808377744e981d2ab1
glennin-codes <glen.nyandiri@students.co.ke>
2022-10-13 15:27:48 +0300

    controlllers

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,15 +1,25 @@
-const data ={};
-data.employees=require('../model/employees.json')
+const data ={
+  employees:require('../model/employees.json'),
+  setEmployees:function(data){
+    this.employees=data 
+  }
+}
 
 
 const readingEmployees= (req,res)=>{
     res.json(data.employees);
   }
   const creatingEmployees=(req,res)=>{
-    res.json({
-            "firstname" :req.body.firstname,
-            "lastname"  : req.body.lastname
-    })
+      const newEmployee={
+        id:data.employees[data.employees.length -1].id+1 || 1,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname
+      }
+      if(!newEmployee.firstname||!newEmployee.lastname){
+        return(res.status(400).json({message:'first and last name are required'}))
+      }
+      data.setEmployees([...data.employees,newEmployee]);
+      res.status(201). json(data.employees)
   }
   const updatingEmployees=(req,res)=>{
     res.json({
